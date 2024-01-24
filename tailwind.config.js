acc182e7b0c1c38d3f9d9a1c64271cda08f56cb9
IbiminaFG <gogoibimina@gmail.com>
2024-01-24 15:40:45 +0100

    added oswald font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
