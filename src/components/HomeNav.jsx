263b13e001fa960278ce66c69af7841b82ec399f
LeRoi Vladimir <banyamkimbi@vrfound.org>
2024-01-24 16:38:02 +0100

    step 1 of navbar

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/Group 1.png'
+import searchImg from '../images/search-01.png'
+import storeImg from '../images/store-add-01.png'
 
 function HomeNav() {
   return (
@@ -12,8 +14,13 @@ function HomeNav() {
                 <span className="self-center whitespace-nowrap">Farmruns</span>
             </Link>
             <div className="flex items-center lg:order-2">
-                <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300  rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
-                <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
+                <Link to="/" className="hover:bg-gray-50 focus:ring-4 focus:ring-[#55A349]  rounded-lg px-3 lg:px-4 py-2 mr-2 focus:outline-none">
+                <img src={searchImg} className="w-[24px]" alt="search" />
+                </Link>
+                <Link to="/" className="hover:bg-gray-50 focus:ring-4 focus:ring-[#55A349]  rounded-lg px-3 lg:px-4 py-2 mr-2 focus:outline-none">
+                <img src={storeImg} className="w-[24px]" alt="store" />
+                </Link>
+                <Link to="/" className="text-white bg-[#55A349] hover:bg-white hover:text-black focus:ring-4 focus:ring-[#55A349] rounded-lg px-3 lg:px-4 py-2 focus:outline-none">Login</Link>
                 <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -23,10 +30,10 @@ function HomeNav() {
             <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                 <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                        <Link to="/" className="block py-2 pr-4 pl-3 text-white rounded bg-[#55A349] lg:bg-transparent lg:text-[#55A349] lg:p-0" aria-current="page">Home</Link>
                     </li>
                     <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Company</a>
+                        <Link to="/" className="block py-2 pr-4 pl-3 text-black border-b border-[#55A349] hover:bg-[#b5f9aa] lg:hover:bg-transparent lg:border-0 lg:hover:text-[#55A349] lg:p-0">Company</Link>
                     </li>
                     <li>
                         <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
