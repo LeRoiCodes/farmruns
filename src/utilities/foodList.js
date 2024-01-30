4c8ee5f49b8ac497420c0d18c602b66949ad657e
LeRoi Vladimir <banyamkimbi@vrfound.org>
2024-01-30 15:44:36 +0100

    finished building the blog food card
    finish building the food blog card
    added new images
    created a blog side bar post card

---
+++ src/utilities/foodList.js
@@ -0,0 +1,42 @@
+import garlic from '../images/Meat.png'
+import jollof from '../images/Garlic.png'
+import potato from '../images/Jollof.png'
+import meatball from '../images/potato.png'
+import ronke from '../images/Ronke.png'
+import susan from '../images/Susan.png'
+
+const foodList = [{
+    id: 1,
+    imageUrl:garlic,
+    name: "Garlic Shrimp Spaghetti",
+    author: "Susan",
+    authorImageUrl: susan,
+    authorTitle: "food Enthusiast", 
+    desc: "Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will sit, without needing to wait for the content to be written and approved. It originally comes from a Latin text, but to today's reader, it's seen as gibberish."
+},{
+    id: 2,
+    imageUrl:jollof,
+    name: "Jollof Rice, Plantain & Chicken",
+    author: "Ronke Robinson",
+    authorImageUrl: ronke,
+    authorTitle: "food Enthusiast", 
+    desc: "Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will sit, without needing to wait for the content to be written and approved. It originally comes from a Latin text, but to today's reader, it's seen as gibberish."
+},{
+    id: 3,
+    imageUrl:potato,
+    name: "Potato Porridge",
+    author: "Susan",
+    authorImageUrl: susan,
+    authorTitle: "food Enthusiast", 
+    desc: "Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will sit, without needing to wait for the content to be written and approved. It originally comes from a Latin text, but to today's reader, it's seen as gibberish."
+},{
+    id: 1,
+    imageUrl:meatball,
+    name: "Meatball Pasta",
+    author: "Ronke",
+    authorImageUrl: ronke,
+    authorTitle: "food Enthusiast", 
+    desc: "Lorem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content. It helps designers plan out where the content will sit, without needing to wait for the content to be written and approved. It originally comes from a Latin text, but to today's reader, it's seen as gibberish."
+},
+]
+ export default foodList
