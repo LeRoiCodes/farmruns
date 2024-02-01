a2d14a4483e493abd7f6734830b19d5e98f3ac85
IbiminaFG <gogoibimina@gmail.com>
2024-02-01 14:16:17 +0100

    create the dashboard page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ import Register from "./pages/Register";
 import RootLayout from "./components/layouts/RootLayout";
 import BlogPage from "./pages/BlogPage";
 import Home from "./pages/consumer/Home";
+import Blog from "./pages/consumer/Blog";
+import Favourites from "./pages/consumer/Favourites";
+import Shop from "./pages/consumer/Shop";
+import Order from "./pages/consumer/Order";
+import FarmersHub from "./pages/consumer/FarmersHub";
+import Settings from "./pages/consumer/Settings";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,7 +25,13 @@ const router = createBrowserRouter(
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<RootLayout />}>
-        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="customer-blog" element={<Blog />} />
+        <Route path="farmers-hub" element={<FarmersHub />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="order" element={<Order />} />
+        <Route path="favourites" element={<Favourites />} />
+        <Route path="settings" element={<Settings />} />
       </Route>
       <Route path="/blog" element={<BlogPage />} />
     </Route>
