8ae5317a8b630dd561156d62b9d087a58e52d20d
IbiminaFG <gogoibimina@gmail.com>
2024-02-09 16:13:33 +0100

    fetch user for merchant

--- src/pages/farmer/Listings.jsx
+++ src/pages/farmer/Listings.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { PiShoppingCart } from "react-icons/pi";
 import { BiStore } from "react-icons/bi";
@@ -71,9 +72,49 @@ const orders = [
 const Listings = () => {
   const { setShowListingModal } = useModal();
 
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        console.error('Token not found');
+        return;
+      }
+
+      try {
+        const response = await fetch('https://food-tech12.onrender.com/api/merchant/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data);
+        } else {
+          console.error('Failed to fetch user details');
+        }
+      } catch (error) {
+        console.error('Error fetching user details:', error);
+      }
+    };
+
+    fetchUserDetails();
+  }, []);
+
   const openModal = () => {
     setShowListingModal(true);
   };
+
+  if(!user){
+    return <div className="w-screen h-screen flex justify-center items-center font-oswald gap-1">
+      <p>Please log in to access page</p>
+      <Link to="/login" className="text-green-10">Login</Link>
+    </div>
+  }
+
   return (
     <section className="bg-[#F0F0F0] h-screen overflow-y-scroll lg:pl-[43px] py-[20px] px-[20px]">
       <div className="flex justify-end">
